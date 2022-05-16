24c0e36b75d368f06e78481cb0060e3ea5410c35
Darsh142001 <darshikapillay6@gmail.com>
2022-05-16 20:01:26 +0200

    Version 1.1. The user can register, login and logout. I did the code for the login so that user can login. I noticed that if i put a white colour background, it doesn't show the text. Need to fix that because the background is white in the register and login page.

--- app/src/main/java/za/ac/iie/opsc/outfit_catalog_st10119438_darshika/ClothesCategory.java
+++ app/src/main/java/za/ac/iie/opsc/outfit_catalog_st10119438_darshika/ClothesCategory.java
@@ -1,17 +1,32 @@
 package za.ac.iie.opsc.outfit_catalog_st10119438_darshika;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ClothesCategory extends AppCompatActivity {
+
+    Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clothes_category_main);
+
+        logout = findViewById(R.id.logoutBtn);
+
     }
 
+    public void logoutClick(View v)
+    {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(ClothesCategory.this, MainActivity.class));
+    }
 
 }
