d000af27d497d5aaff149c7b23fc5d655ecb050d
Darsh142001 <darshikapillay6@gmail.com>
2022-05-23 11:15:30 +0200

    Version 1.2.2
    Fixed the back button in the activity_add_clothes_to_category_main.

--- app/src/main/java/za/ac/iie/opsc/outfit_catalog_st10119438_darshika/AddClothesToCategory.java
+++ app/src/main/java/za/ac/iie/opsc/outfit_catalog_st10119438_darshika/AddClothesToCategory.java
@@ -3,17 +3,22 @@ package za.ac.iie.opsc.outfit_catalog_st10119438_darshika;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddClothesToCategory extends AppCompatActivity implements View.OnClickListener{
 
+    ImageButton backToMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_clothes_to_category_main);
 
+        backToMain = findViewById(R.id.backToMainBtn);
+        backToMain.setOnClickListener(this);
 
     }
 
