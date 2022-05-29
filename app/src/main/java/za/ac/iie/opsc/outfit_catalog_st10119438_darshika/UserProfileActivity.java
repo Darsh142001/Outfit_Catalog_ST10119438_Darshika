8419483276ad8ac731018261f3421145d4d4b348
Darsh142001 <darshikapillay6@gmail.com>
2022-05-29 16:51:19 +0200

    Version 1.2.5
    Now:
    Need to get user full name and email.
    
    Later:
    Need to create a search method. This method will allow user to pick a category from the dropdown menu and when they click search, it will bring back only the clothes that were placed under that specific category.

--- app/src/main/java/za/ac/iie/opsc/outfit_catalog_st10119438_darshika/UserProfileActivity.java
+++ app/src/main/java/za/ac/iie/opsc/outfit_catalog_st10119438_darshika/UserProfileActivity.java
@@ -4,20 +4,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class UserProfileActivity extends AppCompatActivity implements View.OnClickListener{
 
     ImageButton backToMain;
 
+    FirebaseAuth mAuth;
+
+    TextView fullName;
+    TextView email;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_main);
+        mAuth = FirebaseAuth.getInstance();
 
         backToMain = findViewById(R.id.backBtn);
         backToMain.setOnClickListener(this);
+
+        fullName = findViewById(R.id.displayUserNameTV);
+        email = findViewById(R.id.displayUserEmailTV);
+
+        //loadUserDetails();
     }
 
     //This method is called only when the user wants to go back to the activity_main_with_nav_bar.
@@ -29,6 +44,20 @@ public class UserProfileActivity extends AppCompatActivity implements View.OnCli
                 startActivity(new Intent(this, ClothesCategory.class)); //This will go back to the activity with the nav bar.
                 break;
         }
+    }
+
+    //Need to fix!!!
+    String userFullName, userEmail;
+    public void loadUserDetails()
+    {
+        FirebaseUser user = mAuth.getCurrentUser();
+        userFullName = fullName.getText().toString().trim();
+        userEmail = email.getText().toString().trim();
+
+        userFullName = user.getDisplayName();
+        userEmail = user.getEmail();
+
+
     }
 
 
